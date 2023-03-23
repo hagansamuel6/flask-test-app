d1dbb1951c8fc60c6f9c14ab42746fe6c3f7662e
hagansamuel6 <hagansamuel6@gmail.com>
2023-03-23 18:30:22 +0000

    sqlalchamy not working

--- exchange.py
+++ exchange.py
@@ -2,8 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# sqlite db setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/usd-exchange-rate')
